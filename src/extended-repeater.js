b1b519acb7d05126c2b9c4cda20afdf88fc95343
Anastasia <anstasa.li@gmail.com>
2025-01-07 00:33:15 +0300

    feat: add task character count, extended repeat znd transform array

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let assStr = '';
+  if (options.addition !== undefined) {
+    let separator = options.additionSeparator ? options.additionSeparator : '|';
+    let repeat = options.additionRepeatTimes ? options.additionRepeatTimes : 1;
+    assStr = options.addition + separator;
+    assStr = assStr.repeat(repeat).slice(0,-separator.length);
+  }
+  let separator = options.separator ? options.separator : '+';
+  let repeat = options.repeatTimes ? options.repeatTimes : 1;
+  let resultStr = str + assStr + separator;
+
+  return resultStr.repeat(repeat).slice(0,-separator.length);
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -14,26 +14,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function transform(arr) {
-  // try {
-  //   if (arr.indexOf('--double-next') !== -1) {
-  //     const index = arr.indexOf('--double-next');
-  //     return index + 1 > arr.length - 1 ? arr.splice(0, arr.length - 1) : arr.splice(index, 1, arr[index + 1]);
-  //   } else if (arr.indexOf('--double-prev') !== -1) {
-  //     const index = arr.indexOf('--double-prev');
-  //     return index-1 < 0 ? arr.splice(index + 1) : arr.splice(index, 1, arr[index - 1]);
-  //   } else if (arr.indexOf('--discard-next') !== -1) {
-  //     const index = arr.indexOf('--discard-next');
-  //     return index + 1 > arr.length - 1 ? arr.splice(0, arr.length - 1) : arr.splice(index, 2);
-  //   } else if (arr.indexOf('--discard-prev') !== -1) {
-  //     const index = arr.indexOf('--discard-prev');
-  //     return index-1 < 0 ? arr.splice(index + 1) : arr.splice(index - 1, 2);
-  //   }
+  try {
+    if (arr.indexOf('--double-next') !== -1) {
+      const index = arr.indexOf('--double-next');
+      return index + 1 > arr.length - 1 ? arr.splice(0, arr.length - 1) : arr.splice(index, 1, arr[index + 1]);
+    } else if (arr.indexOf('--double-prev') !== -1) {
+      const index = arr.indexOf('--double-prev');
+      return index-1 < 0 ? arr.splice(index + 1) : arr.splice(index, 1, arr[index - 1]);
+    } else if (arr.indexOf('--discard-next') !== -1) {
+      const index = arr.indexOf('--discard-next');
+      return index + 1 > arr.length - 1 ? arr.splice(0, arr.length - 1) : arr.splice(index, 2);
+    } else if (arr.indexOf('--discard-prev') !== -1) {
+      const index = arr.indexOf('--discard-prev');
+      return index-1 < 0 ? arr.splice(index + 1) : arr.splice(index - 1, 2);
+    }
 
-  //   return arr;
-  // } catch (err) {
-  //   throw new Error("'arr' parameter must be an instance of the Array!")
-  // }
-  throw new NotImplementedError('Not implemented');
+    return arr;
+  } catch (err) {
+    throw new Error("'arr' parameter must be an instance of the Array!")
+  }
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
